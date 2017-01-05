82c95bc53d3a24670156f95d981d08948b45fedd
Brian Heim <bheim67@gmail.com>
2017-01-05 15:41:23 -0700

    react router tutorial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { BrowserRouter, Match, Miss } from 'react-router'
+
+import About from './About'
+import Contact from './Contact'
+import NotFound from './NotFound'
+import Navigation from './Navigation'
+import Destination from './destination'
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navigation />
+        <Match exactly pattern="/" component={App} />
+        <Match exactly pattern="/about" component={About} />
+        <Match pattern="/about/:destination" component={Destination} />
+        <Match pattern="/contact" component={Contact} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root />, document.querySelector("#root"))
